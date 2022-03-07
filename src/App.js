bc3239b837574281ca82c4758c675de8227fd4ab
NaginB <naginbanodha@gmail.com>
2022-03-07 16:47:08 +0530

    antd menu added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
-import Sidebar from "./components/Sidebar";
+import "antd/dist/antd.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes> 
-            <Route path="/dashboard" exact element={<Dashboard sidebar={true} />} />
+        <Routes>
+          <Route
+            path="/dashboard"
+            exact
+            element={<Dashboard sidebar={true} />}
+          />
         </Routes>
       </Router>
     </div>
